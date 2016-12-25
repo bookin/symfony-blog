f8e24237e071f6d23b41665a95f60c77dbefc184
Anton <toh1napocht@yandex.ru>
2016-12-25 22:29:15 +0200

    add contact page

--- src/Blog/BlogBundle/Controller/PageController.php
+++ src/Blog/BlogBundle/Controller/PageController.php
@@ -2,7 +2,10 @@
 
 namespace Blog\BlogBundle\Controller;
 
+use Blog\BlogBundle\Entity\Contact;
+use Blog\BlogBundle\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class PageController extends Controller
 {
@@ -15,6 +18,36 @@ class PageController extends Controller
     public function aboutAction()
     {
         return $this->render('BlogBlogBundle:Page:about.html.twig');
+    }
+
+    public function contactAction(Request $request)
+    {
+        $model = new Contact();
+
+        $form = $this->createForm(ContactType::class, $model);
+
+        if($request->isMethod($request::METHOD_POST)){
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                //Send message
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact from my blog')
+                    ->setFrom($model->getEmail())
+                    ->setTo($this->getParameter('blog.admin_email'))
+                    ->setBody($this->renderView('BlogBlogBundle:Email:contact.txt.twig', ['model' => $model]));
+
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->getFlashBag()->add('blog-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
+                return $this->redirect($this->generateUrl('BlogBlogBundle_contact'));
+            }
+        }
+
+        return $this->render('BlogBlogBundle:Page:contact.html.twig', [
+            'form'=>$form->createView()
+        ]);
     }
 
 }
---
+++ src/Blog/BlogBundle/Form/ContactType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Blog\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('subject')
+            ->add('body');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Blog\BlogBundle\Entity\Contact'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'blog_blogbundle_contact';
+    }
+
+
+}
